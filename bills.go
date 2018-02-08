68aafd8cf3c88b04de43a79305d7ee97d2712f47
August Yip <augustyip@gmail.com>
2018-02-08 13:00:35 +0800

    load certification from json file

--- bills.go
+++ bills.go
@@ -5,8 +5,26 @@ import (
   "net/http"
   "net/url"
   "io/ioutil"
+  "encoding/json"
+  "os"
   )
+
+type Certification struct {
+  Service  string
+  Username  string
+  Password  string
+}
+
 func main() {
+
+  file, _ := os.Open("cert.json")
+  decoder := json.NewDecoder(file)
+  certs := make([]Certification, 0)
+  err := decoder.Decode(&certs)
+  if err != nil {
+    fmt.Println("error:", err)
+  }
+
   resp, err := http.PostForm("https://eservice.towngas.com/EAccount/Login/SignIn", url.Values{"LoginID": {"username"},"password": {"password"}})
   if err != nil {
     // handle error
