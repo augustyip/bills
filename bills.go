ff1e6204a130679a6b111bec847e4c2ba91d6173
August Yip <augustyip@gmail.com>
2018-02-22 14:02:44 +0800

    try to use cookies

--- bills.go
+++ bills.go
@@ -7,7 +7,8 @@ import (
   "io/ioutil"
   "encoding/json"
   "os"
-  )
+  "net/http/cookiejar"
+)
 
 type Certification struct {
   Service  string
@@ -25,14 +26,23 @@ func main() {
     fmt.Println("error:", err)
   }
 
+  cookieJar, _ := cookiejar.New(nil)
+  client := &http.Client{
+    Jar: cookieJar,
+  }
+
   for _, cert := range certs {
-    resp, err := http.PostForm("https://eservice.towngas.com/EAccount/Login/SignIn", url.Values{"LoginID": {cert.Username},"password": {cert.Password}})
+    
+    loginResp, err := client.PostForm("https://eservice.towngas.com/EAccount/Login/SignIn", url.Values{"LoginID": {cert.Username},"password": {cert.Password}})
     if err != nil {
       // handle error
     }
+    defer loginResp.Body.Close()
 
-    resp, err := http.Get('https://eservice.towngas.com/en/BillingUsage/NewsNotices')
-    
+    resp, err := http.Get("https://eservice.towngas.com/en/BillingUsage/NewsNotices")
+    if err != nil {
+      // handle error
+    }
     defer resp.Body.Close()
     body, err := ioutil.ReadAll(resp.Body)
     bodyContent := string(body[:])
