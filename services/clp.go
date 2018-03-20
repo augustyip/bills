4de6ea21f4a8988540f50cfa55e5460314dd5883
August Yip <augustyip@gmail.com>
2018-03-20 12:31:18 +0800

    add csrf token for clp request

--- services/clp.go
+++ services/clp.go
@@ -16,7 +16,7 @@ type Clp struct {
 
 // GetServiceDashboard get latest info
 func GetServiceDashboard(c Clp) string {
-
+	var csrfToken string
 	cookieJar, _ := cookiejar.New(nil)
 	client := &http.Client{
 		Jar: cookieJar,
@@ -34,38 +34,35 @@ func GetServiceDashboard(c Clp) string {
 		// handle error
 	}
 	defer cookiesResp.Body.Close()
-	// fmt.Println(loginPageResp.Cookies()[0].String())
-	// for _, cookie := range cookiesResp.Cookies() {
-	// 	fmt.Println(cookie.String())
-	// }
-
-	var clpCookies = cookiesResp.Cookies()
-
-	// loginPageBody, err := ioutil.ReadAll(loginPageResp.Body)
-	// fmt.Println(string(loginPageBody[:]))
+	for _, cookie := range cookiesResp.Cookies() {
+		if cookie.Name == "K2Cie90hi___AntiXsrfToken" {
+			csrfToken = cookie.Value
+		}
+	}
 
 	var loginBody = "username=" + c.Username + "&password=" + c.Password
 	loginReq, err := http.NewRequest("POST", "https://services.clp.com.hk/Service/ServiceLogin.ashx", strings.NewReader(loginBody))
 	if err != nil {
 		// handle error
 	}
-	cookieJar.SetCookies(loginReq.URL, clpCookies)
+
+	loginReq.Header.Set("X-CSRFToken", csrfToken)
+	loginReq.Header.Set("X-Requested-With", "XMLHttpRequest")
+	loginReq.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+
 	loginResp, err := client.Do(loginReq)
 	if err != nil {
 		// handle error
 	}
-	loginRespBody, err := ioutil.ReadAll(loginResp.Body)
-	fmt.Println(string(loginRespBody[:]))
-
 	defer loginResp.Body.Close()
 
 	var loginedCookies = loginResp.Cookies()
 
-	// fmt.Println(loginCookies[0].String())
 	req, err := http.NewRequest("POST", "https://services.clp.com.hk/Service/ServiceDashboard.ashx", strings.NewReader("assCA="))
 
 	cookieJar.SetCookies(req.URL, loginedCookies)
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("X-CSRFToken", csrfToken)
+	req.Header.Set("X-Requested-With", "XMLHttpRequest")
 	resp, err := client.Do(req)
 
 	if err != nil {
